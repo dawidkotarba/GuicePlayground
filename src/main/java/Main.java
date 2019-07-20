532b3dae5966bbff6fb0fb1c0591c2e099d9545e
dawidkotarba <dawidkotarba>
2019-07-20 22:00:28 +0200

    Saving/retrieving protobuf in mock db

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,6 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import controller.ControllerModule;
 import controller.UserController;
-import protobuf.GuicePlaygroundProtos;
-
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
 
 public class Main {
 
@@ -16,22 +11,5 @@ public class Main {
         System.out.println(instance.showUsers());
         System.out.println(instance.showBlogPosts());
 
-        protobufSerializationTest();
-    }
-
-    private static void protobufSerializationTest() {
-        final GuicePlaygroundProtos.User user = GuicePlaygroundProtos.User.newBuilder().setUsername("dawid").setAge(33).build();
-        try (final FileOutputStream fos = new FileOutputStream("User.proto.ser")) {
-            user.writeTo(fos);
-        } catch (final IOException ex) {
-            throw new RuntimeException(ex);
-        }
-
-        try (final FileInputStream fos = new FileInputStream("User.proto.ser")) {
-            final GuicePlaygroundProtos.User userFromFile = GuicePlaygroundProtos.User.parseFrom(fos);
-            System.out.println(userFromFile);
-        } catch (final IOException ex) {
-            throw new RuntimeException(ex);
-        }
     }
 }
--- src/main/java/db/MockDb.java
+++ src/main/java/db/MockDb.java
@@ -1,19 +1,27 @@
 package db;
 
+import com.google.protobuf.AbstractMessageLite;
 import model.BlogPostModel;
 import model.UserModel;
+import protobuf.GuicePlaygroundProtos;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import java.util.ArrayList;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 @Singleton
 public class MockDb implements Database {
 
-    private final List<UserModel> USERS = new ArrayList<>();
-    private final List<BlogPostModel> BLOG_POSTS = new ArrayList<>();
+    private static final String TARGET_FOLDER = "target/";
+    private static final String[] TEST_USERNAMES = {"dawid", "max"};
+    private static final String[] TEST_BLOGPOST_TITLES = {"test1", "test2"};
 
     private final Logger logger;
     private boolean logQueries;
@@ -25,27 +33,65 @@ public class MockDb implements Database {
     }
 
     private void init() {
-        USERS.add(new UserModel("dawid", 33));
-        USERS.add(new UserModel("max", 34));
+        final Random random = new Random();
+        Arrays.asList(TEST_USERNAMES).forEach(username -> saveUser(username, random.nextInt(99)));
+        Arrays.asList(TEST_BLOGPOST_TITLES).forEach(title -> saveBlogPost(title, Integer.toString(random.nextInt(999999))));
+    }
 
-        BLOG_POSTS.add(new BlogPostModel("test1", "test1body"));
-        BLOG_POSTS.add(new BlogPostModel("test2", "test2body"));
+    private void saveUser(final String username, final int age) {
+        final GuicePlaygroundProtos.User user = GuicePlaygroundProtos.User.newBuilder().setUsername(username).setAge(age).build();
+        saveProtobuf(user, username);
+    }
+
+    private void saveBlogPost(final String title, final String body) {
+        final GuicePlaygroundProtos.BlogPost blogPost = GuicePlaygroundProtos.BlogPost.newBuilder().setTitle(title).setBody(body).build();
+        saveProtobuf(blogPost, title);
+    }
+
+    private void saveProtobuf(final AbstractMessageLite protobuf, final String filePrefix) {
+        try (final FileOutputStream fos = new FileOutputStream(TARGET_FOLDER + filePrefix + ".proto.ser")) {
+            protobuf.writeTo(fos);
+        } catch (final IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    private GuicePlaygroundProtos.User readUser(final String filePrefix) {
+        try (final FileInputStream fos = new FileInputStream(TARGET_FOLDER + filePrefix + ".proto.ser")) {
+            return GuicePlaygroundProtos.User.parseFrom(fos);
+        } catch (final IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    private GuicePlaygroundProtos.BlogPost readBlogPost(final String filePrefix) {
+        try (final FileInputStream fos = new FileInputStream(TARGET_FOLDER + filePrefix + ".proto.ser")) {
+            return GuicePlaygroundProtos.BlogPost.parseFrom(fos);
+        } catch (final IOException ex) {
+            throw new RuntimeException(ex);
+        }
     }
 
     @Override
     public List<UserModel> findAllUsers() {
         if (logQueries) {
-            logger.info("Querying: " + USERS);
+            logger.info("Querying: " + Arrays.toString(TEST_USERNAMES));
         }
-        return USERS;
+        return Arrays.stream(TEST_USERNAMES)
+                .map(this::readUser)
+                .map(protobuf -> new UserModel(protobuf.getUsername(), protobuf.getAge()))
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<BlogPostModel> findAllBlogPosts() {
         if (logQueries) {
-            logger.info("Querying: " + BLOG_POSTS);
+            logger.info("Querying: " + Arrays.toString(TEST_BLOGPOST_TITLES));
         }
-        return BLOG_POSTS;
+        return Arrays.stream(TEST_BLOGPOST_TITLES)
+                .map(this::readBlogPost)
+                .map(protobuf -> new BlogPostModel(protobuf.getTitle(), protobuf.getBody()))
+                .collect(Collectors.toList());
     }
 
     @Override
